9141b78016e899cc8b7abe8264792acde1a01ad4
aman.garg <aman.garg@payu.in>
2019-10-23 07:33:38 +0530

    reverse queue using stack

---
+++ queue_ops/reverse_queue.rb
@@ -0,0 +1,14 @@
+def reverse(queue)
+	stack = []
+	while !queue.empty? do
+		popped_item = queue.pop # Remove item from queue
+		stack.push(popped_item) # Insert removed item into stack
+	end
+	while !stack.empty? do
+		popped_item = stack.pop # Remove item from stack
+		queue.unshift(popped_item) # Insert removed item into queue
+	end
+end
+
+queue = [1,2,3,4,5]
+reverse(queue)
